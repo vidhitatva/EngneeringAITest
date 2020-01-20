784c10fcc0c0c8ab477d379b9f3b270aec2b4a12
Zalak Parikh <zalak.parikh@tatvasoft.com>
2020-01-20 10:59:36 +0530

    Project Setup

---
+++ VidhiEngneeringAIDemo/VidhiEngneeringAIDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VidhiEngneeringAIDemo
+//
+//  Created by MAC105 on 20/01/20.
+//  Copyright © 2020 MAC105. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
